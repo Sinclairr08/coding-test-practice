7e6e5562cf45af85eb4957541af8b37dee614eb5
Junhyuk Lee <38907328+Sinclairr08@users.noreply.github.com>
2022-06-18 18:40:12 +0900

    fix: Fix python format

--- baekjoon/1000.py
+++ baekjoon/1000.py
@@ -1,4 +1,5 @@
 import sys
 
-a, b = map(int, sys.stdin.readline().split())
-print(a + b)
+if __name__ == "__main__":
+    a, b = map(int, sys.stdin.readline().split())
+    print(a + b)
--- format.py
+++ format.py
@@ -1,3 +1,5 @@
 import sys
 
-arg1, arg2 = map(int, sys.stdin.readline().split())
+if __name__ == "__main__":
+    arg1, arg2 = map(int, sys.stdin.readline().split())
+    arg = sys.stdin.readline().strip()
